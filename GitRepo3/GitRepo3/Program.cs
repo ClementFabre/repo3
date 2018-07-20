0cbb4becca93c9f9c07bf2d72f56156dd79b514a
NGUYEN <vhnguyen@gmail.com>
2018-07-20 13:52:17 +0200

    modifie par VHN

--- GitRepo3/GitRepo3/Program.cs
+++ GitRepo3/GitRepo3/Program.cs
@@ -14,6 +14,7 @@ namespace GitRepo3
             Console.WriteLine("Git Repo3");
             Console.WriteLine("Version 2");
             Console.WriteLine("By Clement Fabre");
+            Console.WriteLine("Vu, modifie par Viet Hoang NGUYEN");
             Console.Read();
         }
     }
